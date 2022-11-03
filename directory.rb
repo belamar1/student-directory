02c33928f0381a3b180f579cbefe348d063edb47
belamar1 <belamar75@gmail.com>
2022-11-03 16:01:43 +0000

    more refactoring

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
-def input(students)
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty?
-    # add the student hash to the array
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+def interactive_menu
+  # 1. print the menu and ask the user what to do
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+      students = input_students
+    when "2"
+      # show the students
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
   end
-  # return the array of students
-  students
 end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
