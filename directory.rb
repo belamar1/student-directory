6ec5ac6c64cb3afab2da2fd93cd67c25dd8ad342
belamar1 <belamar75@gmail.com>
2022-11-03 11:56:27 +0000

    refactoring

--- directory.rb
+++ directory.rb
@@ -8,3 +8,12 @@ puts "Miss. Sabrina Ogunmola"
 puts "Mr. John van der Merwe"
 puts "Mr. Jushua Funke"
 puts "Aimen Subhi"
+puts "Mr. John Dom"
+puts "Mr. James Bond"
+student_count = 11
+# finally, we print the total as 3 lines below
+# print "Overall, we have "
+# print student_count
+# puts " great students"
+
+puts "Overall, we have #{student_count} great students"
